a31806504102bc5923e456a5eec9ea2e3b0d6d94
VernSG <68459009+VernSG@users.noreply.github.com>
2024-05-17 03:41:27 +0800

    Update projects.tsx

--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -17,11 +17,11 @@ const ICON_SIZE: number = 24;
 export const TECH_STACK_GTPS = [
   {
     label: "C++",
-    icon: <SiCpp size={ICON_SIZE} color="#149FCB" />,
+    icon: <SiIcons.SiCpp size={ICON_SIZE} color="#149FCB" />,
   },
   {
     label: "Vps",
-    icon: <SiVultr size={ICON_SIZE} color="#38BDF8" />,
+    icon: <SiIcons.SiVultr size={ICON_SIZE} color="#38BDF8" />,
   },
 ];
 
