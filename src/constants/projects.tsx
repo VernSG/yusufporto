d1878f9faf0691582b2d96ed00c52d9a221d12e0
VernSG <68459009+VernSG@users.noreply.github.com>
2024-05-17 03:52:38 +0800

    Update projects.tsx

--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -35,32 +35,6 @@ export const TECH_STACK_SIZE = [
   },
 ];
 
-export const TECH_STACK_OOPSUM = [
-  {
-    label: "Next.js",
-    icon: <SiNextdotjs />,
-  },
-  {
-    label: "TypeScript",
-    icon: <SiTypescript size={ICON_SIZE} color="#3078C7" />,
-  },
-  {
-    label: "Tailwind CSS",
-    icon: <SiTailwindcss size={ICON_SIZE} color="#38BDF8" />,
-  },
-];
-
-export const TECH_STACK_COLLOSAL = [
-  {
-    label: "Next.js",
-    icon: <SiNextdotjs />,
-  },
-  {
-    label: "Tailwind CSS",
-    icon: <SiTailwindcss size={ICON_SIZE} color="#38BDF8" />,
-  },
-];
-
 export const PROJECTCARD_CONTENTS = [
   {
     url: "https://youtube.com/VernSG/",
@@ -81,25 +55,5 @@ export const PROJECTCARD_CONTENTS = [
     image:
       "https://res.cloudinary.com/djsdnb4td/image/upload/v1715886791/IMG_20240517_031255_diwabo.jpg",
     imageAlt: "Sizestore Website Screenshot",
-  },
-  {
-    url: "https://oopsum.vercel.app/",
-    name: "Crypto Platform",
-    description:
-      "Web-3 based crypto trading platform, with real-time data provided by CoinGecko Public API. Designed by Kuldeep Jiyani on Figma.",
-    techStack: TECH_STACK_OOPSUM,
-    image:
-      "https://res.cloudinary.com/dqqmzgesp/image/upload/v1697086833/personal-website/projects/oopsum.webp",
-    imageAlt: "Crypto Trading Platform Website Screenshot",
-  },
-  {
-    url: "https://collosal-landing-page-codedesign-dev.vercel.app/",
-    name: "Collosal",
-    description:
-      "Challenge to create landing page from codedesign.dev. Collosal is a visually stunning landing page designed for a software house company.",
-    techStack: TECH_STACK_COLLOSAL,
-    image:
-      "https://res.cloudinary.com/dqqmzgesp/image/upload/v1697086833/personal-website/projects/collosal.webp",
-    imageAlt: "Collosal Website Screenshot",
   },
 ];
