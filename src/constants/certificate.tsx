48183a7513875911a90a8084eda925138e0f7a78
VernSG <68459009+VernSG@users.noreply.github.com>
2024-05-19 19:39:16 +0800

    Update certificate.tsx

--- src/constants/certificate.tsx
+++ src/constants/certificate.tsx
@@ -1,10 +1,13 @@
 import {
     SiFirebase,
     SiFlask,
+    SiNodedotjs,
+    SiJavascript,
     SiMongodb,
     SiNextdotjs,
     SiPython,
     SiPytorch,
+    SiVultr,
     SiReact,
     SiTailwindcss,
     SiTypescript,
@@ -12,118 +15,55 @@ import {
   
   const ICON_SIZE: number = 24;
   
-  export const TECH_STACK_CHATBOT = [
+  export const TECH_STACK_JS = [
     {
-      label: "React",
-      icon: <SiReact size={ICON_SIZE} color="#149FCB" />,
-    },
-    {
-      label: "Tailwind CSS",
-      icon: <SiTailwindcss size={ICON_SIZE} color="#38BDF8" />,
-    },
-    {
-      label: "Python",
-      icon: <SiPython size={ICON_SIZE} color="#FEDE59" />,
-    },
-    {
-      label: "PyTorch",
-      icon: <SiPytorch size={ICON_SIZE} color="#EE4D2D" />,
-    },
-    {
-      label: "Flask",
-      icon: <SiFlask size={ICON_SIZE} color="#63BDC8" />,
-    },
-    {
-      label: "Firebase",
-      icon: <SiFirebase size={ICON_SIZE} color="#FFCA2F" />,
+      label: "Javascript",
+      icon: <SiJavascript size={ICON_SIZE} color="#38BDF8" />,
     },
   ];
   
-  export const TECH_STACK_X_APP = [
+  export const TECH_STACK_AWS = [
     {
-      label: "Next.js",
-      icon: <SiNextdotjs />,
-    },
-    {
-      label: "TypeScript",
-      icon: <SiTypescript size={ICON_SIZE} color="#3078C7" />,
-    },
-    {
-      label: "Tailwind CSS",
-      icon: <SiTailwindcss size={ICON_SIZE} color="#38BDF8" />,
-    },
-    {
-      label: "MongoDB Atlas",
-      icon: <SiMongodb size={ICON_SIZE} color="#016949" />,
+      label: "AWS",
+      icon: <SiVultr size={ICON_SIZE} color="#38BDF8" />,
     },
   ];
-  
-  export const TECH_STACK_OOPSUM = [
-    {
-      label: "Next.js",
-      icon: <SiNextdotjs />,
-    },
-    {
-      label: "TypeScript",
-      icon: <SiTypescript size={ICON_SIZE} color="#3078C7" />,
-    },
-    {
-      label: "Tailwind CSS",
-      icon: <SiTailwindcss size={ICON_SIZE} color="#38BDF8" />,
-    },
-  ];
-  
-  export const TECH_STACK_COLLOSAL = [
-    {
-      label: "Next.js",
-      icon: <SiNextdotjs />,
-    },
-    {
-      label: "Tailwind CSS",
-      icon: <SiTailwindcss size={ICON_SIZE} color="#38BDF8" />,
-    },
+   export const TECH_STACK_BE = [
+        {
+          label: "Node JS",
+          icon: <SiNodedotjs size={ICON_SIZE} color="#38BDF8" />,
+        },
   ];
   
   export const PROJECTCARD_CONTENTS = [
     {
-      url: "https://chatbot-app.pages.dev/",
-      name: "AI Chatbot",
+      url: "https://www.dicoding.com/certificates/0LZ0269LNX65",
+      name: "Introduction to Programming using Javascript",
       description:
-        "An Indonesian language chatbot with deep learning and NLP, that can answer user questions regarding university enrollment information.",
-      techStack: TECH_STACK_CHATBOT,
+        "Diterbitkan Jan 2024 Kadaluarsa Jan 2027.",
+      techStack: TECH_STACK_JS,
       image:
-        "https://res.cloudinary.com/dqqmzgesp/image/upload/v1697086786/personal-website/projects/chatbot.webp",
-      imageAlt: "AI Chatbot Website Screenshot",
+        "https://res.cloudinary.com/djsdnb4td/image/upload/v1716118348/js_kgmcg2.png",
+      imageAlt: "Javascript",
     },
     {
-      url: "https://x-app-clone.vercel.app/",
-      name: "X App Clone",
+      url: "https://www.dicoding.com/certificates/72ZDV7OWQZYW",
+      name: "Cloud Practitioner Essentials",
       description:
-        "This is a project for developing a clone application of X formerly Twitter, although the end result may not be entirely identical to the X application.",
-      techStack: TECH_STACK_X_APP,
+        "Diterbitkan Mei 2024 Kadaluarsa Mei 2027",
+      techStack: TECH_STACK_AWS,
       image:
-        "https://res.cloudinary.com/dqqmzgesp/image/upload/v1697086832/personal-website/projects/x_app_clone.webp",
-      imageAlt: "X App Clone Website Screenshot",
+        "https://res.cloudinary.com/djsdnb4td/image/upload/v1716118331/aws_zqzgbk.png",
+      imageAlt: "Dicoding Website Screenshot",
     },
     {
-      url: "https://oopsum.vercel.app/",
-      name: "Crypto Platform",
-      description:
-        "Web-3 based crypto trading platform, with real-time data provided by CoinGecko Public API. Designed by Kuldeep Jiyani on Figma.",
-      techStack: TECH_STACK_OOPSUM,
-      image:
-        "https://res.cloudinary.com/dqqmzgesp/image/upload/v1697086833/personal-website/projects/oopsum.webp",
-      imageAlt: "Crypto Trading Platform Website Screenshot",
-    },
-    {
-      url: "https://collosal-landing-page-codedesign-dev.vercel.app/",
-      name: "Collosal",
-      description:
-        "Challenge to create landing page from codedesign.dev. Collosal is a visually stunning landing page designed for a software house company.",
-      techStack: TECH_STACK_COLLOSAL,
-      image:
-        "https://res.cloudinary.com/dqqmzgesp/image/upload/v1697086833/personal-website/projects/collosal.webp",
-      imageAlt: "Collosal Website Screenshot",
-    },
+        url: "https://www.dicoding.com/certificates/KEXL1L53YXG2",
+        name: "Belajar Membuat Aplikasi Back-End untuk Pemula",
+        description:
+          "Diterbitkan Mei 2024 Kadaluarsa Mei 2027",
+        techStack: TECH_STACK_BE,
+        image:
+          "https://res.cloudinary.com/djsdnb4td/image/upload/v1716118332/be_o69t8x.png",
+        imageAlt: "Dicoding Website Screenshot",
+      },
   ];
-  
