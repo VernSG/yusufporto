7fe415bd861796f3c3ee5c071ab380d6c84ccd75
VernSG <68459009+VernSG@users.noreply.github.com>
2024-05-31 22:21:06 +0800

    Update Callsign.tsx

--- src/components/cards/Callsign.tsx
+++ src/components/cards/Callsign.tsx
@@ -39,7 +39,7 @@ export default function Callsign({ display }: { display: string }) {
         ) : (
           <Image
   className="h-auto w-auto"
-  src={userData?.data?.discord_user?.avatar ?? '{PROFILE_PICTURE_URL}'} // Ganti '/path/to/default/avatar' dengan path default avatar Anda
+  src={userData?.data?.discord_user?.avatar ?? PROFILE_PICTURE_URL} // Ganti '/path/to/default/avatar' dengan path default avatar Anda
  // Mengakses avatar dari objek discord_user
   alt="DISCORD_PROFILE"
   width={64}
