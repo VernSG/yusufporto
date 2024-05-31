e434b988543cd9f2c900c606691a6bd85d0ae39b
VernSG <68459009+VernSG@users.noreply.github.com>
2024-05-31 15:28:59 +0800

    Create discordUser.js

---
+++ src/app/api/discordUser.js
@@ -0,0 +1,40 @@
+const getUserData = async (discordUserId) => {
+  try {
+    // Mengambil data pengguna Discord dari API Lanyard
+    const rawRes = await fetch(`https://api.lanyard.rest/v1/users/${discordUserId}`);
+    const { data } = await rawRes.json();
+
+    // Mengubah status Discord menjadi representasi yang lebih mudah dibaca
+    let statusBeautify;
+    switch (data.discord_status) {
+      case "online":
+        statusBeautify = data.active_on_discord_mobile ? "Online (Mobile)" : "Online";
+        data.discord_status = data.active_on_discord_mobile ? "online-mobile" : "online";
+        break;
+
+      case "offline":
+        statusBeautify = "Offline";
+        break;
+
+      case "idle":
+        statusBeautify = "Idle";
+        break;
+
+      case "dnd":
+        statusBeautify = "Do not disturb!";
+        break;
+
+      default:
+        break;
+    }
+
+    // Mengembalikan data pengguna Discord beserta status yang sudah diubah
+    return { data, statusBeautify };
+  } catch (error) {
+    // Menghandle kesalahan jika terjadi
+    console.error("Error fetching Discord user data:", error);
+    throw new Error("Failed to fetch Discord user data");
+  }
+};
+
+export default getUserData;
