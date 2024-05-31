e76a97e32301da6b9eb5f18ea41ce645070463c1
VernSG <68459009+VernSG@users.noreply.github.com>
2024-05-31 22:15:52 +0800

    Update Callsign.tsx

--- src/components/cards/Callsign.tsx
+++ src/components/cards/Callsign.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { SiDiscord } from "react-icons/si";
 import { ApiResponse } from "@/app/api/discordUser";
-import {PROFILE_PICTURE_URL} from "@/contstans/index";
+import {PROFILE_PICTURE_URL} from "@/contstans";
 import getUserData from "@/app/api/discordUser"; // Import fungsi getUserData
 
 export default function Callsign({ display }: { display: string }) {
