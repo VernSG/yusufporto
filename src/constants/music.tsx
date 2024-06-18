9a8e379e798a7e8073698279a306790e4db1a5a8
Muhammad Yusuf <68459009+VernSG@users.noreply.github.com>
2024-06-19 01:10:40 +0800

    changes: music lib

--- src/constants/music.tsx
+++ src/constants/music.tsx
@@ -1,12 +1,21 @@
 export const MUSICCARD_CONTENTS = [
     {
-      name: "Last Child - Sekuat Hatimu",
+      name: "Yaosobi - Tabun",
       description:
-        "Lagu ini menceritakan penyesalan penyanyi yang telah menyia-nyiakan masa mudanya dan memilih jalan hidup yang bertentangan dengan sang ibu.",
+        "tells of someone's feelings of regret and doubt about the decisions they've made in life, as well as a curiosity about how their life would have changed if they had chosen a different path. The song reflects on wasted time and choices made, with the hope of finding deeper meaning and purpose in life.",
       image:
-        "https://res.cloudinary.com/djsdnb4td/image/upload/v1718440667/lastchild_tbkiix.jpg",
-      imageAlt: "Screenshot Tutorial GTPS",
-      audioSrc: "https://res.cloudinary.com/djsdnb4td/video/upload/v1718436371/videoplayback_rwxku0.mp4", // URL audio untuk Project 1
+        "https://res.cloudinary.com/djsdnb4td/image/upload/v1718730338/yaosobi_szycza.jpg",
+      imageAlt: "Yaosobi",
+      audioSrc: "https://res.cloudinary.com/djsdnb4td/video/upload/v1718730176/videoplayback_1_cwni0y.mp4", // URL audio untuk Music 1
+    },
+    {
+      name: "Yuika - Sukidakara",
+      description:
+        "depicts deep feelings of love and a strong desire to possess someone. In its lyrics, there is a passage that portrays someone expressing the wish to protect their beloved from the evils of the world:",
+      image:
+        "https://res.cloudinary.com/djsdnb4td/image/upload/v1718730527/Yuika_-_Suki_Dakara_Lyrics_Translation_wuzsp5.jpg",
+      imageAlt: "Yaosobi",
+      audioSrc: "https://res.cloudinary.com/djsdnb4td/video/upload/v1718730487/Y2meta.app_-_%E5%A5%BD%E3%81%8D%E3%81%A0%E3%81%8B%E3%82%89___%E3%83%A6%E3%82%A4%E3%82%AB_MV_128_kbps_xtlg6k.mp3", // URL audio untuk Music 1
     },
     // Tambahkan proyek lain dengan audioSrc sesuai kebutuhan
   ];
