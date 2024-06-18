e880ad5fe7d927006c99411e5dd41d7777f76221
Muhammad Yusuf <68459009+VernSG@users.noreply.github.com>
2024-06-19 01:42:42 +0800

    Update MusicCard.tsx

--- src/components/cards/MusicCard.tsx
+++ src/components/cards/MusicCard.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import * as React from "react";
