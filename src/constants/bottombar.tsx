3dcb20f5029b4e4c4c309c1e42170609ce55af35
Muhammad Yusuf <68459009+VernSG@users.noreply.github.com>
2024-06-22 14:41:57 +0800

    update: change bottombar icon

--- src/constants/bottombar.tsx
+++ src/constants/bottombar.tsx
@@ -1,8 +1,8 @@
 import { AiOutlineHome } from "react-icons/ai";
 import { FaRegPaperPlane } from "react-icons/fa";
-import { GoProjectSymlink } from "react-icons/go";
+import { GoProject } from "react-icons/go";
 import { SiAboutdotme } from "react-icons/si";
-import { TbWriting } from "react-icons/tb";
+import { GrCertificate } from "react-icons/gr";
 import { CiMusicNote1 } from "react-icons/ci";
 
 const ICON_SIZE: number = 24;
@@ -10,8 +10,8 @@ const ICON_SIZE: number = 24;
 export const BOTTOMBAR_ITEMS = [
   { icon: <AiOutlineHome size={ICON_SIZE} />, pathname: "/" },
   { icon: <SiAboutdotme size={ICON_SIZE} />, pathname: "/about" },
-  { icon: <GoProjectSymlink size={ICON_SIZE} />, pathname: "/projects" },
+  { icon: <GoProject size={ICON_SIZE} />, pathname: "/projects" },
   { icon: <CiMusicNote1 size={ICON_SIZE} />, pathname: "/music" },
-  { icon: <TbWriting size={ICON_SIZE} />, pathname: "/certificate" },
+  { icon: <GrCertificate size={ICON_SIZE} />, pathname: "/certificate" },
   { icon: <FaRegPaperPlane size={ICON_SIZE} />, pathname: "/contact" },
 ];
