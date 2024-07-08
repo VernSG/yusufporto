c5a7c6c305a87e8f2919f4a0e1afc4e450a41954
Muhammad Yusuf <68459009+VernSG@users.noreply.github.com>
2024-07-08 13:39:36 +0800

    Update layout.tsx

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -47,25 +47,12 @@ export default function RootLayout({
     <html lang={DEFAULT_METADATA.locale}>
       <Head>
         <title>{DEFAULT_METADATA.creator}</title>
-        <meta name="description" content={DEFAULT_METADATA.description} />
-        <meta name="keywords" content={DEFAULT_METADATA.keyword} />
-        <meta name="robots" content={DEFAULT_METADATA.robots} />
-        <meta name="creator" content={DEFAULT_METADATA.creator} />
-        <meta name="author" content={DEFAULT_METADATA.creator} />
-        <meta name="author" content={DEFAULT_METADATA.url} />
-        <meta property="og:url" content={DEFAULT_METADATA.openGraph.url.toString()} />
-        <meta property="og:type" content={DEFAULT_METADATA.openGraph.type} />
-        <meta property="og:site_name" content={DEFAULT_METADATA.openGraph.siteName} />
-        <meta property="og:description" content={DEFAULT_METADATA.openGraph.description} />
-        <meta property="og:title" content={DEFAULT_METADATA.openGraph.title} />
-        <meta property="og:image" content={DEFAULT_METADATA.openGraph.images[0].url} />
-        <meta property="og:locale" content={DEFAULT_METADATA.openGraph.locale} />
-        <meta name="twitter:card" content={DEFAULT_METADATA.twitter.card} />
-        <meta name="twitter:creator" content={DEFAULT_METADATA.twitter.creator} />
-        <meta name="twitter:site" content={DEFAULT_METADATA.twitter.site} />
-        <meta name="twitter:title" content={DEFAULT_METADATA.twitter.title} />
-        <meta name="twitter:description" content={DEFAULT_METADATA.twitter.description} />
-        <meta name="twitter:image" content={DEFAULT_METADATA.twitter.images[0].url} />
+        <meta name="description" content={DEFAULT_METADATA.description ?? 'Default description'} />
+        <meta name="keywords" content={DEFAULT_METADATA.keywords ?? 'default, keywords'} />
+        <meta name="robots" content={DEFAULT_METADATA.robots ?? 'index, follow'} />
+        <meta name="creator" content={DEFAULT_METADATA.creator ?? 'Default Creator'} />
+        <meta name="author" content={DEFAULT_METADATA.authors?.name ?? 'Default Author'} />
+        <meta name="author" content={DEFAULT_METADATA.authors?.url ?? 'https://default.url'} />
       </Head>
       <body className={inter.className}>
         <Providers>
