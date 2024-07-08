a8bf04d2bcb9dce8a2246afe309d9b9392c300b1
Muhammad Yusuf <68459009+VernSG@users.noreply.github.com>
2024-07-08 13:23:22 +0800

    Update layout.tsx

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -44,7 +44,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang={DEFAULT_METADATA.locale}>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="robots" content={metadata.robots} />
+        <meta name="creator" content={metadata.creator} />
+        <meta name="author" content={metadata.authors.name} />
+        <meta name="author" content={metadata.authors.url} />
+        <meta property="og:url" content={metadata.openGraph.url.toString()} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta property="og:locale" content={metadata.openGraph.locale} />
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:creator" content={metadata.twitter.creator} />
+        <meta name="twitter:site" content={metadata.twitter.site} />
+        <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta name="twitter:description" content={metadata.twitter.description} />
+        <meta name="twitter:image" content={metadata.twitter.images[0].url} />
+      </Head>
       <body className={inter.className}>
         <Providers>
           <Layouts>{children}</Layouts>
