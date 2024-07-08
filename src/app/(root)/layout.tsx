b2067ff813e90d95429b923892fc6d8c99806380
Muhammad Yusuf <68459009+VernSG@users.noreply.github.com>
2024-07-08 13:20:53 +0800

    Update layout.tsx

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,16 +1,16 @@
 import Layouts from "@/components/layouts";
 import Providers from "@/components/layouts/Providers";
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Inter } from "next/font/google";
-import Head from "next/head"; // Import Head from next/head
 import "../globals.css";
 import { DEFAULT_METADATA } from "@/constants/metadata";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(DEFAULT_METADATA.url),
-  title: DEFAULT_METADATA.creator, // Pastikan DEFAULT_METADATA.creator memiliki tipe yang tepat
+  metadataBase: new URL("https://yusufs.me"),
+  title: DEFAULT_METADATA.creator,
   robots: DEFAULT_METADATA.robots,
   keywords: DEFAULT_METADATA.keyword,
   description: DEFAULT_METADATA.description,
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     siteName: DEFAULT_METADATA.siteName,
     description: DEFAULT_METADATA.description,
     title: DEFAULT_METADATA.creator,
-    images: [{ url: DEFAULT_METADATA.image }],
+    images: DEFAULT_METADATA.image,
     locale: DEFAULT_METADATA.locale,
   },
   twitter: {
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
     site: DEFAULT_METADATA.siteName,
     title: DEFAULT_METADATA.creator,
     description: DEFAULT_METADATA.description,
-    images: [{ url: DEFAULT_METADATA.image }],
+    images: DEFAULT_METADATA.image,
   },
 };
 
@@ -44,29 +44,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang={DEFAULT_METADATA.locale}>
-      <Head>
-        <title>{metadata.title ?? 'Default Title'}</title>
-        <meta name="description" content={metadata.description ?? 'Default description'} />
-        <meta name="keywords" content={metadata.keywords ?? 'default, keywords'} />
-        <meta name="robots" content={metadata.robots ?? 'index, follow'} />
-        <meta name="creator" content={metadata.creator ?? 'Default Creator'} />
-        <meta name="author" content={metadata.authors?.name ?? 'Default Author'} />
-        <meta name="author" content={metadata.authors?.url ?? 'https://default.url'} />
-        <meta property="og:url" content={metadata.openGraph?.url?.toString() ?? 'https://default.url'} />
-        <meta property="og:type" content={metadata.openGraph?.type ?? 'website'} />
-        <meta property="og:site_name" content={metadata.openGraph?.siteName ?? 'Default Site Name'} />
-        <meta property="og:description" content={metadata.openGraph?.description ?? 'Default description'} />
-        <meta property="og:title" content={metadata.openGraph?.title ?? 'Default Title'} />
-        <meta property="og:image" content={metadata.openGraph?.images?.[0]?.url ?? '/defaultImage.png'} />
-        <meta property="og:locale" content={metadata.openGraph?.locale ?? 'en-US'} />
-        <meta name="twitter:card" content={metadata.twitter?.card ?? 'summary'} />
-        <meta name="twitter:creator" content={metadata.twitter?.creator ?? 'Default Creator'} />
-        <meta name="twitter:site" content={metadata.twitter?.site ?? '@defaultsite'} />
-        <meta name="twitter:title" content={metadata.twitter?.title ?? 'Default Title'} />
-        <meta name="twitter:description" content={metadata.twitter?.description ?? 'Default description'} />
-        <meta name="twitter:image" content={metadata.twitter?.images?.[0]?.url ?? '/defaultImage.png'} />
-      </Head>
+    <html lang="en">
       <body className={inter.className}>
         <Providers>
           <Layouts>{children}</Layouts>
