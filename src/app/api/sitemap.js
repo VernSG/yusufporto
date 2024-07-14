9752ffdafc9e72e57396fecc2e3767a4c2f02e30
Muhammad Yusuf <68459009+VernSG@users.noreply.github.com>
2024-07-15 03:00:52 +0800

    Delete src/app/api/sitemap.js

--- src/app/api/sitemap.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const filePath = path.resolve('public', 'sitemap.xml');
-  res.setHeader('Content-Type', 'application/xml');
-  res.status(200).sendFile(filePath);
-}
