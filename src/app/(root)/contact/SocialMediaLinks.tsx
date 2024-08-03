a9dae4d31a19a16a522834e4af707e56da5d706d
Muhammad Yusuf <68459009+VernSG@users.noreply.github.com>
2024-08-03 18:28:24 +0800

    fix: fixed gradient color not appearing on mapped elements

--- src/app/(root)/contact/SocialMediaLinks.tsx
+++ src/app/(root)/contact/SocialMediaLinks.tsx
@@ -3,7 +3,6 @@ import SocialMediaCard from "@/components/cards/SocialMediaCard";
 import BreakLine from "@/components/elements/BreakLine";
 import { CONTACT_ITEMS } from "@/constants/contact";
 import { motion } from "framer-motion";
-import React from "react"; // Pastikan Anda mengimpor React
 
 export default function SocialMediaLinks() {
   return (
@@ -16,20 +15,17 @@ export default function SocialMediaLinks() {
 
       <ul className="mt-8 flex flex-col gap-4 sm:flex-row sm:flex-wrap">
         {CONTACT_ITEMS.map((item, index) => (
-          <li key={index} className="w-full sm:w-auto">
-            <SocialMediaCard
-              url={item.url}
-              ariaLabel={item.label}
-              icon={item.icon}
-              label={item.label}
-              backgroundColor={item.backgroundColor}
-            />
-          </li>
+          <SocialMediaCard
+            key={index}
+            url={item.url}
+            ariaLabel={item.label}
+            icon={item.icon}
+            label={item.label}
+            backgroundColor={item.backgroundColor}
+          />
         ))}
-        <li className="w-full sm:w-auto">
-          <BreakLine />
-        </li>
       </ul>
+      <BreakLine />
     </motion.section>
   );
 }
