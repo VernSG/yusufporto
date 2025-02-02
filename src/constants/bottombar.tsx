4313922a5f52142ff934450eb55d090c84ec08fd
VernSG <snexmania76@gmail.com>
2025-02-03 03:39:14 +0800

    feat(constant): add new icon for bottom bar and sidebar
    Change-Id: Iac655386e1134cfb756454de51a9fd0cf5032ba0

--- src/constants/bottombar.tsx
+++ src/constants/bottombar.tsx
@@ -1,4 +1,4 @@
-import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineHome, AiOutlineProject } from "react-icons/ai";
 import { FaRegPaperPlane } from "react-icons/fa";
 import { GoProject } from "react-icons/go";
 import { SiAboutdotme } from "react-icons/si";
@@ -8,7 +8,7 @@ import { GrCertificate } from "react-icons/gr";
 import { CiMusicNote1 } from "react-icons/ci";
 import { FaBlog } from "react-icons/fa";
 
-const ICON_SIZE: number = 24;
+const ICON_SIZE: number = 22;
 
 export const BOTTOMBAR_ITEMS = [
   { icon: <AiOutlineHome size={ICON_SIZE} />, pathname: "/" },
@@ -17,6 +17,7 @@ export const BOTTOMBAR_ITEMS = [
     icon: <FaBlog size={ICON_SIZE} />,
     pathname: "/blog",
   },
+  { icon: <AiOutlineProject size={ICON_SIZE} />, pathname: "/products" },
   { icon: <BiSolidDashboard size={ICON_SIZE} />, pathname: "/dashboard" },
   { icon: <CiChat1 size={ICON_SIZE} />, pathname: "/guestbook" },
   { icon: <FaRegPaperPlane size={ICON_SIZE} />, pathname: "/contact" },
