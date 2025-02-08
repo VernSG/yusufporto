6407c37d91e6d76ab1c4456080854a8f006d705b
VernSG <snexmania76@gmail.com>
2025-02-09 04:57:27 +0800

    style: modified text desc
    Change-Id: I61679accbde5cabc624419719075a023f0957928

--- src/app/(root)/chat/page.tsx
+++ src/app/(root)/chat/page.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect, useRef } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import PageTitle from "@/components/elements/PageTitle";
 
 export default function ChatPage() {
   const [input, setInput] = useState("");
@@ -15,9 +16,9 @@ export default function ChatPage() {
   const [isTyping, setIsTyping] = useState(false);
   const [isCooldown, setIsCooldown] = useState(false);
   const [commands] = useState([
-    { command: "/about-yusuf", description: "Penjelasan tentang Yusuf" },
-    { command: "/help", description: "Dapatkan bantuan tentang fitur AI" },
-    { command: "/clear", description: "Bersihkan semua chat di layar" },
+    { command: "/about-yusuf", description: "About Yusuf" },
+    { command: "/help", description: "Show command feature" },
+    { command: "/clear", description: "Clear all chat" },
   ]);
   const messagesEndRef = useRef<null | HTMLDivElement>(null);
 
@@ -67,10 +68,9 @@ export default function ChatPage() {
 
     if (input.toLowerCase() === "/about-yusuf") {
       botResponse =
-        "Yusuf adalah seorang pengembang berbakat yang memiliki keahlian dalam membangun aplikasi AI dan bot Discord.";
+        "Yusuf is a 19-year-old web developer specializing in React.js and Node.js. He is passionate about building modern and scalable web applications using Next.js, Express.js, and MongoDB. He is also interested in server management, including VPS configuration and deployment.";
     } else if (input.toLowerCase() === "/help") {
-      botResponse =
-        "Daftar command yang tersedia: /about-yusuf, /help, /clear.";
+      botResponse = "List command: /about-yusuf, /help, /clear.";
     } else if (input.toLowerCase() === "/clear") {
       setMessages([]);
       setIsTyping(false);
@@ -99,78 +99,81 @@ export default function ChatPage() {
   };
 
   return (
-    <div className="mt-8 flex h-screen flex-col rounded-lg bg-gray-100">
-      <div className="flex-1 space-y-4 overflow-y-auto p-4">
-        {messages.map((msg, index) => (
-          <div
-            key={index}
-            className={`max-w-md rounded-lg p-4 text-sm shadow-md md:max-w-lg ${
-              msg.role === "user"
-                ? "ml-auto bg-indigo-500 text-white"
-                : "bg-white text-gray-800"
+    <div className="p-8">
+      <PageTitle title="Chat Bot" description="Chat with Open AI" />
+      <div className="mt-8 flex h-screen flex-col rounded-lg bg-gray-100">
+        <div className="flex-1 space-y-4 overflow-y-auto p-4">
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`max-w-md rounded-lg p-4 text-sm shadow-md md:max-w-lg ${
+                msg.role === "user"
+                  ? "ml-auto bg-indigo-500 text-white"
+                  : "bg-white text-gray-800"
+              }`}
+              style={{
+                wordWrap: "break-word",
+                overflowWrap: "break-word",
+                maxWidth: "90%",
+              }}
+            >
+              <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                {msg.content}
+              </ReactMarkdown>
+            </div>
+          ))}
+          {isTyping && (
+            <div className="max-w-md animate-pulse rounded-lg bg-gray-200 p-3 text-gray-500 shadow-md md:max-w-lg">
+              Bot sedang mengetik...
+            </div>
+          )}
+          <div ref={messagesEndRef}></div>
+        </div>
+
+        <div className="relative flex items-center border-t border-gray-300 bg-white p-4">
+          <input
+            type="text"
+            value={input}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+            placeholder={
+              isCooldown
+                ? "Tunggu sebentar sebelum mengirim pesan..."
+                : "Tulis pesan atau gunakan command seperti /about-yusuf"
+            }
+            className="w-full rounded border border-gray-300 px-4 py-2 text-sm shadow-sm outline-none focus:ring-2 focus:ring-indigo-500"
+            disabled={isCooldown}
+          />
+          <button
+            onClick={sendMessage}
+            className={`ml-2 rounded-full px-4 py-2 text-sm font-medium text-white shadow-md ${
+              isCooldown
+                ? "cursor-not-allowed bg-gray-400"
+                : "bg-indigo-600 hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-400"
             }`}
-            style={{
-              wordWrap: "break-word",
-              overflowWrap: "break-word",
-              maxWidth: "90%",
-            }}
+            disabled={isCooldown}
           >
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
-              {msg.content}
-            </ReactMarkdown>
-          </div>
-        ))}
-        {isTyping && (
-          <div className="max-w-md animate-pulse rounded-lg bg-gray-200 p-3 text-gray-500 shadow-md md:max-w-lg">
-            Bot sedang mengetik...
+            Kirim
+          </button>
+        </div>
+
+        {showSuggestions && (
+          <div className="absolute bottom-full mb-2 w-full rounded-lg border border-gray-300 bg-white shadow-lg">
+            {commands
+              .filter((cmd) => cmd.command.startsWith(input))
+              .map((cmd, index) => (
+                <div
+                  key={index}
+                  onClick={() => selectCommand(cmd.command)}
+                  className="cursor-pointer px-4 py-2 hover:bg-gray-100"
+                >
+                  <p className="font-semibold text-gray-700">{cmd.command}</p>
+                  <p className="text-xs text-gray-500">{cmd.description}</p>
+                </div>
+              ))}
           </div>
         )}
-        <div ref={messagesEndRef}></div>
       </div>
-
-      <div className="relative flex items-center border-t border-gray-300 bg-white p-4">
-        <input
-          type="text"
-          value={input}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-          placeholder={
-            isCooldown
-              ? "Tunggu sebentar sebelum mengirim pesan..."
-              : "Tulis pesan atau gunakan command seperti /about-yusuf"
-          }
-          className="w-full rounded border border-gray-300 px-4 py-2 text-sm shadow-sm outline-none focus:ring-2 focus:ring-indigo-500"
-          disabled={isCooldown}
-        />
-        <button
-          onClick={sendMessage}
-          className={`ml-2 rounded-full px-4 py-2 text-sm font-medium text-white shadow-md ${
-            isCooldown
-              ? "cursor-not-allowed bg-gray-400"
-              : "bg-indigo-600 hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-400"
-          }`}
-          disabled={isCooldown}
-        >
-          Kirim
-        </button>
-      </div>
-
-      {showSuggestions && (
-        <div className="absolute bottom-full mb-2 w-full rounded-lg border border-gray-300 bg-white shadow-lg">
-          {commands
-            .filter((cmd) => cmd.command.startsWith(input))
-            .map((cmd, index) => (
-              <div
-                key={index}
-                onClick={() => selectCommand(cmd.command)}
-                className="cursor-pointer px-4 py-2 hover:bg-gray-100"
-              >
-                <p className="font-semibold text-gray-700">{cmd.command}</p>
-                <p className="text-xs text-gray-500">{cmd.description}</p>
-              </div>
-            ))}
-        </div>
-      )}
     </div>
   );
 }
