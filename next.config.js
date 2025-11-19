329790b578848fec43b61c7433413282e5cd4e4a
Your Name <snexmania76@gmail.com>
2025-11-20 05:57:29 +0800

    chore: fix configurations domains deprecated in next 13+
    Change-Id: I3876ac00d2511455eb3a42bf4df9c6fee9b37559

--- next.config.js
+++ next.config.js
@@ -1,12 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    domains: [
-      "res.cloudinary.com",
-      "i.scdn.co",
-      "cdn.sanity.io",
-      "cdn.discordapp.com",
-      "asset.cloudinary.com",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "asset.cloudinary.com",
+        pathname: "/**",
+      },
     ],
-  };
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
 
 module.exports = nextConfig;
